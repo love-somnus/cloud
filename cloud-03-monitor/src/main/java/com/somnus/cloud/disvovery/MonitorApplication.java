c94c0355b5a11ee02394eba9146b9b11665ae126
love-somnus <928200207@qq.com>
2018-10-26 16:23:26 +0800

    love-somnus <928200207@qq.com>

--- cloud-03-monitor/src/main/java/com/somnus/cloud/disvovery/MonitorApplication.java
+++ cloud-03-monitor/src/main/java/com/somnus/cloud/disvovery/MonitorApplication.java
@@ -1,9 +1,27 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.somnus.cloud.disvovery;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.ListConfig;
 import com.hazelcast.config.MapConfig;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -15,7 +33,7 @@ import org.springframework.context.annotation.Bean;
 
 /**
  * @ClassName: MonitorApplication
- * @Description: The class cloud monitor application.
+ * @Description: 微服务监控中心
  * @author Somnus
  * @date 2018年9月26日
  */
@@ -24,6 +42,7 @@ import org.springframework.context.annotation.Bean;
 @EnableHystrixDashboard
 @EnableHystrix
 @EnableCircuitBreaker
+@EnableAdminServer
 @SpringBootApplication
 public class MonitorApplication {
 
